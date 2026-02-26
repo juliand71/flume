4d75ea205bdf2d37685342c69f31141e89310844
Julian Dixon <julian.dixon1@va.gov>
2026-02-26 14:52:34 -0500

    set up xcode project

---
+++ ios/Flume/Flume/FlumeApp.swift
@@ -0,0 +1,32 @@
+//
+//  FlumeApp.swift
+//  Flume
+//
+//  Created by Julian Dixon on 2/26/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct FlumeApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ ios/Flume/FlumeTests/FlumeTests.swift
@@ -0,0 +1,16 @@
+//
+//  FlumeTests.swift
+//  FlumeTests
+//
+//  Created by Julian Dixon on 2/26/26.
+//
+
+import Testing
+
+struct FlumeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
